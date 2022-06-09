dc8fd43919623f69ba4b9c4c3c4c5dce40f589e3
alekart <94320656+DiabloPro@users.noreply.github.com>
2022-06-08 19:25:30 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,19 +1,58 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/DiabloPro/UiLibrary/main/Main.lua"))()
 local RunService = game:GetService("RunService")
 local ContextActionService = game:GetService("ContextActionService")
+local HttpService = game:GetService("HttpService")
 local Assets = game:GetObjects("rbxassetid://9839966208")[1]
 
 local screenGUI = Library.init("Kart's Epsilon 2")
 local player = game.Players.LocalPlayer
-local connections = {
-    ["trinketEvent"] = nil,
-    ["speedEvent"] = nil,
-    ["manaPrecent"] = nil,
-    ["health"] = nil,
-    ["healthBars"] = {},
-    ["manaBars"] = {},
-    ["toolView"] = {}
+local settings = {
+    speedToggle = false,
+    speed = 20,
+    viewHealthBars = false,
+    viewManaBars = false,
+    viewTools = false,
 }
+local connections = {
+    trinketEvent = nil,
+    speedEvent = nil,
+    manaPrecent = nil,
+    health = nil,
+    healthBars = {},
+    manaBars = {},
+    toolView = {},
+    infiniteMana = nil,
+    noFog = nil,
+    day = nil
+}
+local spellPrecentages = {
+    Gate = {}
+}
+
+local defaultSettings = {
+    speedBind = nil
+}
+
+local savedSettings = {}
+if writefile then
+    if isfile("KartEpsilon.txt") then
+        savedSettings = HttpService:JSONDecode(readfile("KartEpsilon.txt"))
+    else
+        savedSettings = defaultSettings
+    end
+end
+local settingsMT = setmetatable(savedSettings,{
+    __newindex = function(self, key, value)
+        rawset(self, key, value)
+        if writefile then
+            print("saving")
+            writefile("KartEpsilon.txt", HttpService:JSONEncode(savedSettings))
+        end
+    end,
+    __index = function(self, index)
+        return rawget(self, index)
+    end
+})
 
 local function getParent(screenGUI)
     if syn then
@@ -31,10 +70,9 @@ local humanoidTab = screenGUI:createTab("http://www.roblox.com/asset/?id=9827813
 
 local localPlayerSection = humanoidTab:createSection("Local Player")
 
-local speedToggle = false
 local toggleSpeed = localPlayerSection:createToggle("Walk Speed", function(boolean)
-    speedToggle = boolean
-    if not speedToggle then
+    settings.speedToggle = boolean
+    if not settings.speedToggle then
         if game.ReplicatedStorage.Outfits[player.Data.Armor.Value] and game.ReplicatedStorage.Outfits[player.Data.Armor.Value].Stats:FindFirstChild("SpeedBoost") then
             player.Character.Stats.WalkSpeed.Value = 20 + game.ReplicatedStorage.Outfits[player.Data.Armor.Value].Stats.SpeedBoost.Value
         elseif game.ReplicatedStorage.OldOutfits[player.Data.Armor.Value] and game.ReplicatedStorage.OldOutfits[player.Data.Armor.Value].Stats:FindFirstChild("SpeedBoost") then
@@ -50,17 +88,87 @@ toggleSpeed:createSlider({0,80}, 20, false, function(value)
         connections.speedEvent:Disconnect()
     end
     connections.speedEvent = player.Character.Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
-        if speedToggle then
+        if settings.speedToggle then
             player.Character.Humanoid.WalkSpeed = value
             player.Character.Stats.WalkSpeed.Value = value
         end
     end)
 end)
 
+toggleSpeed:createBind(function(bind)
+    savedSettings.speedBind = Enum.KeyCode[bind]
+end)
+
+if savedSettings.speedBind then
+    toggleSpeed:setBind(savedSettings.speedBind)
+end
+
+
 local noFall = localPlayerSection:createToggle("No Fall", function(boolean)
     player.Character.FallDamage.Disabled = boolean
 end)
 
+local localPlayerVisuals = humanoidTab:createSection("Visuals")
+
+local noFog = localPlayerVisuals:createToggle("No Fog", function(boolean)
+    if boolean then
+        game.Lighting.FogEnd = 9e9
+        game.Lighting.FogStart = 9e9
+        connections.noFog = game.Lighting:GetPropertyChangedSignal("FogEnd"):Connect(function()
+            game.Lighting.FogEnd = 9e9
+            game.Lighting.FogStart = 9e9
+        end)
+    else
+        connections.noFog:Disconnect()
+    end
+end)
+
+local day = localPlayerVisuals:createToggle("Day Time", function(boolean)
+    if boolean then
+        game.Lighting.TimeOfDay = 12
+        connections.day = game.Lighting:GetPropertyChangedSignal("TimeOfDay"):Connect(function()
+            game.Lighting.TimeOfDay = 12
+        end)
+    else
+        connections.day:Disconnect()
+    end
+end)
+
+local regionColor = localPlayerVisuals:createToggle("Region Color", function(boolean)
+    if boolean then
+        game.Lighting.regionColor.Enabled = false
+    else
+        game.Lighting.regionColor.Enabled = true
+    end
+end)
+
+
+local manaSection = humanoidTab:createSection("Mana")
+local manaGui
+
+local manaPrecentage = manaSection:createToggle("Mana Precentage", function(boolean)
+    if boolean then
+        manaGui = Instance.new("ScreenGui")
+        getParent(manaGui)
+        local textLabel = Instance.new("TextLabel")
+        textLabel.BackgroundTransparency = 1
+        textLabel.Font = Enum.Font.SourceSansLight
+        textLabel.TextSize = 20
+        textLabel.Size = UDim2.new(0, 20, 0, 20)
+        textLabel.Parent = manaGui
+        textLabel.TextColor3 = Color3.fromRGB(255,255,255)
+        textLabel.TextStrokeTransparency = .9
+        textLabel.Position = player.PlayerGui.ManaGui.LeftContainer.Position + UDim2.new(0,26,0,-178)
+        textLabel.AnchorPoint = player.PlayerGui.ManaGui.LeftContainer.AnchorPoint
+        textLabel.Text = math.floor(player.Character.Stats.Mana.Value).."%"
+        connections.manaPrecent = player.Character.Stats.Mana.Changed:Connect(function(newValue)
+            manaGui.TextLabel.Text = math.floor(newValue).."%"
+        end)
+    else
+        connections.manaPrecent:Disconnect()
+        manaGui:Destroy()
+    end
+end)
 -- trinkets
 local trinketTab = screenGUI:createTab("http://www.roblox.com/asset/?id=9830996211")
 local trinketSettingsSection = trinketTab:createSection("Trinket Settings")
@@ -88,72 +196,17 @@ end)
 -- combat
 local combatTab = screenGUI:createTab("http://www.roblox.com/asset/?id=9839226250")
 local visualCombatSection = combatTab:createSection("Visuals")
-
-local manaGui
-local manaPrecentage = visualCombatSection:createToggle("Mana Precentage", function(boolean)
-    if boolean then
-        manaGui = Instance.new("ScreenGui")
-        getParent(manaGui)
-        local textLabel = Instance.new("TextLabel")
-        textLabel.BackgroundTransparency = 1
-        textLabel.Font = Enum.Font.SourceSansLight
-        textLabel.TextSize = 20
-        textLabel.Size = UDim2.new(0, 20, 0, 20)
-        textLabel.Parent = manaGui
-        textLabel.TextColor3 = Color3.fromRGB(255,255,255)
-        textLabel.TextStrokeTransparency = .9
-        textLabel.Position = player.PlayerGui.ManaGui.LeftContainer.Position + UDim2.new(0,26,0,-178)
-        textLabel.AnchorPoint = player.PlayerGui.ManaGui.LeftContainer.AnchorPoint
-        textLabel.Text = math.floor(player.Character.Stats.Mana.Value).."%"
-        connections.manaPrecent = player.Character.Stats.Mana.Changed:Connect(function(newValue)
-            manaGui.TextLabel.Text = math.floor(newValue).."%"
-        end)
-    else
-        connections.manaPrecent:Disconnect()
-        manaGui:Destroy()
-    end
-end)
-
-local healthGui
-local healthViewer = visualCombatSection:createToggle("Health", function(boolean)
-    if boolean then
-        healthGui = Instance.new("ScreenGui")
-        getParent(healthGui)
-        local textLabel = Instance.new("TextLabel")
-        textLabel.BackgroundTransparency = 1
-        textLabel.Font = Enum.Font.SourceSansLight
-        textLabel.TextSize = 20
-        textLabel.Size = UDim2.new(0, 20, 0, 20)
-        textLabel.Parent = healthGui
-        textLabel.TextColor3 = Color3.fromRGB(255,255,255)
-        textLabel.TextStrokeTransparency = .9
-        textLabel.Position = player.PlayerGui.StatGui.Container.Position + UDim2.new(0, 10, 0, -1)
-        textLabel.AnchorPoint = player.PlayerGui.StatGui.Container.AnchorPoint
-        textLabel.Text = math.floor(player.Character.Humanoid.Health).." / "..player.Character.Humanoid.MaxHealth
-        connections.health = player.Character.Humanoid.HealthChanged:Connect(function()
-            healthGui.TextLabel.Text = math.floor(player.Character.Humanoid.Health).." / "..player.Character.Humanoid.MaxHealth
-        end)
-    else
-        connections.health:Disconnect()
-        healthGui:Destroy()
-    end
-end)
-
-
-local viewHealthBars = false
-local viewManaBars = false
-local viewTools = false
 local healthGuis = {}
 local manaGuis = {}
 local toolGuis = {}
 
 local function createHealthBar(character)
-    local torso = character:FindFirstChild("Torso")
-    if torso then
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    if humanoidRootPart then
         local healthBar = Assets.Health:Clone()
         healthGuis[#healthGuis + 1] = healthBar
         getParent(healthBar)
-        healthBar.Adornee = character.Torso
+        healthBar.Adornee = humanoidRootPart
         healthBar.Frame.Frame.Size = UDim2.new(character.Humanoid.Health / character.Humanoid.MaxHealth, 0, 1, 0)
 
         connections.healthBars[#connections.healthBars + 1] =  character.Humanoid.HealthChanged:Connect(function()
@@ -163,12 +216,12 @@ local function createHealthBar(character)
 end
 
 local function createManaBar(character)
-    local torso = character:FindFirstChild("Torso")
-    if torso then
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    if humanoidRootPart then
         local manaBar = Assets.Mana:Clone()
         manaGuis[#manaGuis + 1] = manaBar
         getParent(manaBar)
-        manaBar.Adornee = character.Torso
+        manaBar.Adornee = humanoidRootPart
         manaBar.Frame.Frame.Size = UDim2.new(1, 0, character.Stats.Mana.Value / 100, 0)
 
         connections.manaBars[#connections.manaBars + 1] = character.Stats.Mana.Changed:Connect(function()
@@ -178,14 +231,14 @@ local function createManaBar(character)
 end
 
 local function createToolViewer(character)
-    local torso = character:FindFirstChild("Torso")
-    if torso then
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    if humanoidRootPart then
         local toolBar = Assets.Tool:Clone()
         toolGuis[#toolGuis + 1] = toolBar
         getParent(toolBar)
-        toolBar.Adornee = character.Torso
+        toolBar.Adornee = humanoidRootPart
         local tool = character:FindFirstChildWhichIsA("Tool") 
-        if character:FindFirstChildWhichIsA("Tool") then
+        if tool then
             toolBar.Frame.Text = tool.Name
         else
             toolBar.Frame.Text = ""
@@ -204,31 +257,31 @@ local function createToolViewer(character)
     end
 end
 
-game.Players.PlayerAdded:Connect(function(newPlayer)
-    newPlayer.CharacterAdded:Connect(function()
-        newPlayer.CharacterAdded:Wait()
-        if viewHealthBars then
-            createHealthBar(newPlayer.Character)
-        end
-        if viewManaBars then
-            createManaBar(newPlayer.Character)
-        end
-        if viewTools then
-            createToolViewer(newPlayer.Character)
-        end
-    end)
+game.Workspace.Alive.ChildAdded:Connect(function(child)
+    if settings.viewHealthBars then
+        createHealthBar(child)
+    end
+    if settings.viewManaBars then
+        createManaBar(child)
+    end
+    if settings.viewTools then
+        createToolViewer(child)
+    end
 end)
 
 local healthBars = visualCombatSection:createToggle("Health Bars", function(boolean)
     if boolean then
-        viewHealthBars = true
-        for i,v in pairs(game.Players:GetChildren()) do
-            if v ~= player then
-                createHealthBar(v.Character)
+        settings.viewHealthBars = boolean
+        for i,v in pairs(workspace.Alive:GetChildren()) do
+            if v ~= player.Character then
+                local playerFromCharacter = game.Players:GetPlayerFromCharacter(v)
+                if playerFromCharacter then
+                    createHealthBar(v)
+                end
             end
         end
     else
-        viewHealthBars = false
+        settings.viewHealthBars = boolean
         for i,v in pairs(connections.healthBars) do
             if v.Connected then
                 v:Disconnect()
@@ -243,14 +296,17 @@ end)
 
 local manaBars = visualCombatSection:createToggle("Mana Bars", function(boolean)
     if boolean then
-        viewManaBars = true
-        for i,v in pairs(game.Players:GetChildren()) do
-            if v ~= player then
-                createManaBar(v.Character)
+        settings.viewManaBars = boolean
+        for i,v in pairs(workspace.Alive:GetChildren()) do
+            if v ~= player.Character then
+                local playerFromCharacter = game.Players:GetPlayerFromCharacter(v)
+                if playerFromCharacter then
+                    createManaBar(v)
+                end  
             end
         end
     else
-        viewManaBars = false
+        settings.viewManaBars = boolean
         for i,v in pairs(connections.manaBars) do
             if v.Connected then
                 v:Disconnect()
@@ -265,14 +321,17 @@ end)
 
 local viewTools = visualCombatSection:createToggle("View Tools", function(boolean)
     if boolean then
-        viewTools = true
-        for i,v in pairs(game.Players:GetChildren()) do
-            if v ~= player then
-                createToolViewer(v.Character)
+        settings.viewTools = boolean
+        for i,v in pairs(workspace.Alive:GetChildren()) do
+            if v ~= player.Character then
+                local playerFromCharacter = game.Players:GetPlayerFromCharacter(v)
+                if playerFromCharacter then
+                    createToolViewer(v)
+                end  
             end
         end
     else
-        viewTools = false
+        settings.viewTools = boolean
         for i,v in pairs(connections.toolView) do
             if v.Connected then
                 v:Disconnect()
@@ -284,6 +343,7 @@ local viewTools = visualCombatSection:createToggle("View Tools", function(boolea
         end
     end
 end)
+
 
 --leaderboard viewer
 local currentHover = {}
@@ -323,7 +383,7 @@ end)
 local function spectate(actionName, inputState, inputObject)
     if inputState == Enum.UserInputState.Begin and currentHover[1] then
         local spectatePlayer = game.Players:FindFirstChild(currentHover[1])
-        if spectatePlayer then
+        if spectatePlayer and spectatePlayer.Character then
             if spectatePlayer.Character:FindFirstChild("Humanoid") then
                 workspace.CurrentCamera.CameraSubject = spectatePlayer.Character.Humanoid
             end
